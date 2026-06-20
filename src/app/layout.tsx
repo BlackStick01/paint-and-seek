538b8a4dab6f8ae349de5f14d83e802b6f7693ae
BlackStick01 <71808004+BlackStick01@users.noreply.github.com>
2026-06-20 10:16:49 +0800

    Add Google Analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,20 @@ import type { ReactNode } from 'react';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z6FSBQFFBK" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z6FSBQFFBK');
+`
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
